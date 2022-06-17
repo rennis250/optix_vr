dfce2f3ee481def06b9f2783bf8cfc11864d07f8
Robert J. Ennis <rje@sdf.org>
2022-06-17 20:35:23 +0200

    trying to get basic cuda-gl interop to work

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "rob_optix.h"
-#include "SDL.h"
+#include "rob_sdl.h"
 
 // this include may only appear in a single source file:
 #include <optix_function_table_definition.h>
@@ -7,15 +7,37 @@
 #include <iostream>
 #include <fstream>
 
+#include "exceptions.h"
+
 const unsigned int width = 256;
 const unsigned int height = 256;
 
 int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
+    auto sdlApp = rob::SDLApp(width, height);
     auto optState = rob::OptixState<uchar4>();
+
+    GLuint m_pbo;
+    glGenBuffers(1, &m_pbo);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, m_pbo);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * width * height * sizeof(GLubyte), 0, GL_STREAM_DRAW);
+    // glBindBuffer(GL_ARRAY_BUFFER, m_pbo);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(uchar4) * (size_t)width * (size_t)height, nullptr, GL_STREAM_DRAW);
+    // glBindBuffer(GL_ARRAY_BUFFER, 0u);
+    // glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0u);
+
+    GLuint tex = 0;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    cudaGraphicsResource* m_cuda_gfx_resource = nullptr;
+    CUDA_CHECK(
+        cudaGraphicsGLRegisterBuffer(
+            &m_cuda_gfx_resource,
+            m_pbo,
+            cudaGraphicsMapFlagsWriteDiscard
+        )
+    );
 
     const std::vector<float3> vertices = {
         { -0.5f, -0.5f, 0.0f },
@@ -45,11 +67,11 @@ int main(int argc, char* argv[]) {
 
     optState.build_sbt();
 
-    auto d_image = rob::CudaBuffer<uchar4>(width, height);
-    
+    // auto d_image = rob::CudaBuffer<uchar4>(width, height);
+
     // Populate the per-launch params
     Params params;
-    params.image = d_image.get_device_ptr();
+    // params.image = d_image.get_device_ptr();
     params.image_width = width;
     params.image_height = height;
     params.cam_eye = make_float3(0.0f, 0.0f, 2.0f);
@@ -57,31 +79,62 @@ int main(int argc, char* argv[]) {
     params.cam_v = make_float3(0.0f, 1.0f, 0.0f);
     params.cam_w = make_float3(0.0f, 0.0f, -2.0f);
 
+    uchar4* gl_image = 0;
+    size_t buffer_size = 0u;
+    CUDA_CHECK(
+        cudaGraphicsMapResources(1, &m_cuda_gfx_resource, 0)
+    );
+    CUDA_CHECK(
+        cudaGraphicsResourceGetMappedPointer(
+            reinterpret_cast<void**>(&gl_image),
+            &buffer_size,
+            m_cuda_gfx_resource
+        )
+    );
+    params.image = gl_image;
+
     optState.upload_params(params);
+
     optState.render();
 
-    d_image.download();
-    std::vector<uchar4> host_pixels = d_image.get_host_data();
+    // d_image.download();
+    // std::vector<uchar4> host_pixels = d_image.get_host_data();
 
     // Output FB as Image
-    std::ofstream MyFile("test.ppm");
-    MyFile << "P3\n" << width << " " << height << "\n255\n";
-    for (int j = (int)height - 1; j >= 0; j--) {
-        for (int i = 0; i < (int)width; i++) {
-            size_t pixel_index = j * (size_t)width + i;
+     std::ofstream MyFile("test.ppm");
+     MyFile << "P3\n" << width << " " << height << "\n255\n";
+     for (int j = (int)height - 1; j >= 0; j--) {
+         for (int i = 0; i < (int)width; i++) {
+             size_t pixel_index = j * (size_t)width + i;
             
-            int ir = static_cast<int>(host_pixels[pixel_index].x);
-            int ig = static_cast<int>(host_pixels[pixel_index].y);
-            int ib = static_cast<int>(host_pixels[pixel_index].z);
+             int ir = static_cast<int>(host_pixels[pixel_index].x);
+             int ig = static_cast<int>(host_pixels[pixel_index].y);
+             int ib = static_cast<int>(host_pixels[pixel_index].z);
 
-            MyFile << ir << " " << ig << " " << ib << "\n";
-        }
+             MyFile << ir << " " << ig << " " << ib << "\n";
+         }
+     }
+     MyFile.close();
+
+    cudaGraphicsUnmapResources(1, &m_cuda_gfx_resource, 0);
+
+    while (1) {
+        sdlApp.clearScreen();
+        sdlApp.registerInput();
+        sdlApp.drawScene();
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+        glEnable(GL_TEXTURE_2D);
+        /*glBegin(GL_QUADS);
+            glTexCoord2f(0.0f, 0.0f); glVertex2f(0, 0);
+            glTexCoord2f(0.0f, 1.0f); glVertex2f(0, H);
+            glTexCoord2f(1.0f, 1.0f); glVertex2f(W, H);
+            glTexCoord2f(1.0f, 0.0f); glVertex2f(W, 0);
+        glEnd();*/
+        glDisable(GL_TEXTURE_2D);
+
+        SDL_Delay(16);
     }
-    MyFile.close();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return 0;
 }
---
+++ rob_sdl.h
@@ -0,0 +1,119 @@
+#pragma once
+
+// #include <gl/GL.h>
+#include "glad/glad.h"
+
+#include "SDL.h"
+#include "SDL_opengl.h"
+
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include <stdbool.h>
+
+namespace rob {
+	class SDLApp {
+	public:
+		SDLApp(unsigned int width, unsigned int height);
+		~SDLApp();
+
+		void registerInput();
+		void clearScreen();
+		void drawScene();
+
+	private:
+		SDL_Window* m_window = nullptr;
+		SDL_Renderer* m_renderer = nullptr;
+		SDL_GLContext m_gl_context = {};
+
+		unsigned int m_width = 0;
+		unsigned int m_height = 0;
+	};
+
+	SDLApp::SDLApp(unsigned int width, unsigned int height) {
+		m_width = width;
+		m_height = height;
+
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+			std::cerr << "shit sdl" << std::endl;
+			exit(1);
+		}
+
+		SDL_GL_LoadLibrary(NULL); // Default OpenGL is fine.
+		
+		// Request an OpenGL 4.5 context (should be core)
+		SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		
+		m_window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, m_width, m_height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+		if (!m_window) {
+			std::cerr << "shit window" << std::endl;
+			exit(1);
+		}
+
+		m_gl_context = SDL_GL_CreateContext(m_window);
+
+		m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if (!m_renderer) {
+			std::cerr << "shit renderer" << std::endl;
+			exit(1);
+		}
+
+		SDL_GL_SetSwapInterval(1);
+
+		// Check OpenGL properties
+		printf("OpenGL loaded\n");
+		gladLoadGLLoader(SDL_GL_GetProcAddress);
+		printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+		printf("Renderer: %s\n", glGetString(GL_RENDERER));
+		printf("Version:  %s\n", glGetString(GL_VERSION));
+
+		// Disable depth test and face culling.
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_CULL_FACE);
+
+		glViewport(0, 0, m_width, m_height);
+	}
+
+	SDLApp::~SDLApp() {
+		SDL_DestroyRenderer(m_renderer);
+		SDL_DestroyWindow(m_window);
+		SDL_Quit();
+	}
+
+	void SDLApp::registerInput() {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				exit(0);
+				break;
+			default:
+				break;
+			}
+		}
+
+		return;
+	}
+
+	void SDLApp::clearScreen() {
+		// SDL_SetRenderDrawColor(m_renderer, 96, 128, 255, 255);
+		// SDL_RenderClear(m_renderer);
+
+		glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	void SDLApp::drawScene() {
+		// SDL_RenderPresent(m_renderer);
+
+		SDL_GL_SwapWindow(m_window);
+	}
+}
